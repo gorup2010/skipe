f351bd73f98b636a13b13c3164d627a9c87eaaad
Hoi Lau <lauhoi2010@gmail.com>
2025-03-25 15:58:51 +0700

    Ignore fe folder in github action

---
+++ src/it/java/com/chatapp/skipe/controller/AuthControllerIT.java
@@ -0,0 +1,42 @@
+package com.chatapp.skipe.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.chatapp.skipe.configuration.TestcontainersConfig;
+import com.chatapp.skipe.service.UserService;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Import(TestcontainersConfig.class)
+class AuthControllerIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockitoBean
+    UserService userService;
+
+    @Test
+    void testGetEndpoint() throws Exception {
+        mockMvc.perform(get("/test"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello"));
+    }
+}
+
